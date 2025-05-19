47739a4b7bb017d85079df6ebbc69439389238e7
DELL <DELL@DESKTOP-NVG5UJR>
2025-05-19 20:07:42 +0530

    First Commit

---
+++ SDET/src/Assignments/Task_01.java
@@ -0,0 +1,19 @@
+package Assignments;
+
+public class Task_01 {
+
+	public static void main(String[] args) {
+
+ int a=1;
+ int b=2;
+ int max= (a>b) ? a : b;
+ System.out.println("Max number is=" +max);
+ 
+ int min= (a<b) ? a : b;
+ System.out.println("Min number is=" +min);
+ 
+		 
+
+	}
+
+}
